de4b2847934501273d3e6c01d26459f965fbab6b
KyleKatarn <kylekatarnls@gmail.com>
2018-11-01 15:04:19 +0100

    Setup unit tests

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # business-time
+
 Carbon mixin to handle business days and opening hours
+
+[![Latest Stable Version](https://poser.pugx.org/cmixin/business-time/v/stable.png)](https://packagist.org/packages/cmixin/business-time)
+[![Build Status](https://travis-ci.org/kylekatarnls/business-time.svg?branch=master)](https://travis-ci.org/kylekatarnls/business-time)
+[![Code Climate](https://codeclimate.com/github/kylekatarnls/business-time/badges/gpa.svg)](https://codeclimate.com/github/kylekatarnls/business-time)
+[![Test Coverage](https://codeclimate.com/github/kylekatarnls/business-time/badges/coverage.svg)](https://codeclimate.com/github/kylekatarnls/business-time/coverage)
+[![Issue Count](https://codeclimate.com/github/kylekatarnls/business-time/badges/issue_count.svg)](https://codeclimate.com/github/kylekatarnls/business-time)
+[![StyleCI](https://styleci.io/repos/129502391/shield?branch=master)](https://styleci.io/repos/129502391)
+
+## Install
+
+```
+composer require cmixin/business-time
+```
+
+## Usage
+
+First load the mixin in some global bootstrap place of your app:
+
+```php
+<?php
+
+use Cmixin\BusinessTime;
+
+BusinessTime::enable('Carbon\Carbon');
+// Or if you use Laravel:
+// BusinessDay::enable('Illuminate\Support\Carbon');
+
+// As a second argument you can set default opening hours:
+BusinessTime::enable('Carbon\Carbon', [
+  'monday' => ['09:00-12:00', '13:00-18:00'],
+  'tuesday' => ['09:00-12:00', '13:00-18:00'],
+  'wednesday' => ['09:00-12:00'],
+  'thursday' => ['09:00-12:00', '13:00-18:00'],
+  'friday' => ['09:00-12:00', '13:00-20:00'],
+  'saturday' => ['09:00-12:00', '13:00-16:00'],
+  'sunday' => [],
+  'exceptions' => [
+    '2016-11-11' => ['09:00-12:00'],
+    '2016-12-25' => [],
+    '01-01' => [], // Recurring on each 1st of january
+    '12-25' => ['09:00-12:00'], // Recurring on each 25th of december
+  ],
+]);
+```
+
+Business days methods are now available on any Carbon instance
+used anywhere later.
--- tests/Cmixin/BusinessTimeTest.php
+++ tests/Cmixin/BusinessTimeTest.php
@@ -10,7 +10,8 @@ class BusinessTimeTest extends TestCase
 
     protected function setUp()
     {
-        BusinessTime::enable(static::CARBON_CLASS, [
+        $carbon = static::CARBON_CLASS;
+        BusinessTime::enable($carbon, [
             'monday' => ['09:00-12:00', '13:00-18:00'],
             'tuesday' => ['09:00-12:00', '13:00-18:00'],
             'wednesday' => ['09:00-12:00'],
@@ -25,7 +26,6 @@ class BusinessTimeTest extends TestCase
                 '12-25' => ['09:00-12:00'], // Recurring on each 25th of december
             ],
         ]);
-        $carbon = static::CARBON_CLASS;
         $carbon::resetHolidays();
     }
 
@@ -34,7 +34,61 @@ class BusinessTimeTest extends TestCase
         $carbon = static::CARBON_CLASS;
         $this->assertTrue($carbon::isOpenOn('monday'));
         $this->assertFalse($carbon::isOpenOn('sunday'));
+    }
+
+    public function testIsClosedOn()
+    {
+        $carbon = static::CARBON_CLASS;
         $this->assertTrue($carbon::isClosedOn('sunday'));
         $this->assertFalse($carbon::isClosedOn('monday'));
+    }
+
+    public function testOpeningHours()
+    {
+        $carbon = static::CARBON_CLASS;
+        $carbon::setOpeningHours([
+            $carbon::SUNDAY => ['08:00-10:40'],
+        ]);
+        $date = $carbon::now()->setOpeningHours([
+            $carbon::TUESDAY => ['12:30-15:00'],
+        ]);
+        $this->assertTrue($carbon::isOpenOn('sunday'));
+        $this->assertTrue($carbon::isClosedOn('tuesday'));
+        $this->assertTrue($date->isOpenOn('tuesday'));
+        $this->assertTrue($date->isClosedOn('sunday'));
+
+        $carbon::setOpeningHours([
+            -1 => ['08:00-10:40'],
+        ]);
+        $this->assertTrue($carbon::isOpenOn('saturday'));
+        $this->assertTrue($carbon::isClosedOn('sunday'));
+    }
+
+    public function testNeutralEnable()
+    {
+        $carbon = static::CARBON_CLASS;
+        $this->assertTrue($carbon::enable());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Opening hours parameter should be a Spatie\OpeningHours\OpeningHours instance or an array.
+     */
+    public function testConvertOpeningHours()
+    {
+        $carbon = static::CARBON_CLASS;
+        $carbon::convertOpeningHours($carbon::now());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Opening hours has not be set.
+     */
+    public function testUndefinedOpeningHours()
+    {
+        $carbon = static::CARBON_CLASS;
+        $carbon::resetMacros();
+        BusinessTime::enable($carbon);
+        $carbon::getOpeningHours();
     }
 }
