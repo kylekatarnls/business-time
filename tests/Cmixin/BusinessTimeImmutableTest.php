3d989eb4bade609ba73940c549aafcb120fea730
KyleK <kylekatarnls@gmail.com>
2018-11-01 23:26:09 +0100

    Implement nextOpenExcludingHolidays and nextCloseIncludingHolidays

--- tests/Cmixin/BusinessTimeImmutableTest.php
+++ tests/Cmixin/BusinessTimeImmutableTest.php
@@ -7,4 +7,14 @@ use Carbon\CarbonImmutable;
 class BusinessTimeImmutableTest extends BusinessTimeTest
 {
     const CARBON_CLASS = CarbonImmutable::class;
+
+    public function testMutability()
+    {
+        $carbon = static::CARBON_CLASS;
+        $date = $carbon::now();
+        $this->assertNotSame($date, $date->nextOpen());
+        $this->assertNotSame($date, $date->nextClose());
+        $this->assertNotSame($date, $date->nextOpenExcludingHolidays());
+        $this->assertNotSame($date, $date->nextCloseIncludingHolidays());
+    }
 }
