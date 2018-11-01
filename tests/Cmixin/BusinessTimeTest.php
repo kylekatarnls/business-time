2b9aeca377237ef1b7062d75ef7c43c9685de3da
KyleKatarn <kylekatarnls@gmail.com>
2018-11-01 15:50:57 +0100

    Test code style

--- tests/Cmixin/BusinessTimeTest.php
+++ tests/Cmixin/BusinessTimeTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tests\Cmixin;
 
 use Cmixin\BusinessTime;
@@ -12,18 +13,18 @@ class BusinessTimeTest extends TestCase
     {
         $carbon = static::CARBON_CLASS;
         BusinessTime::enable($carbon, [
-            'monday' => ['09:00-12:00', '13:00-18:00'],
-            'tuesday' => ['09:00-12:00', '13:00-18:00'],
-            'wednesday' => ['09:00-12:00'],
-            'thursday' => ['09:00-12:00', '13:00-18:00'],
-            'friday' => ['09:00-12:00', '13:00-20:00'],
-            'saturday' => ['09:00-12:00', '13:00-16:00'],
-            'sunday' => [],
+            'monday'     => ['09:00-12:00', '13:00-18:00'],
+            'tuesday'    => ['09:00-12:00', '13:00-18:00'],
+            'wednesday'  => ['09:00-12:00'],
+            'thursday'   => ['09:00-12:00', '13:00-18:00'],
+            'friday'     => ['09:00-12:00', '13:00-20:00'],
+            'saturday'   => ['09:00-12:00', '13:00-16:00'],
+            'sunday'     => [],
             'exceptions' => [
                 '2016-11-11' => ['09:00-12:00'],
                 '2016-12-25' => [],
-                '01-01' => [], // Recurring on each 1st of january
-                '12-25' => ['09:00-12:00'], // Recurring on each 25th of december
+                '01-01'      => [], // Recurring on each 1st of january
+                '12-25'      => ['09:00-12:00'], // Recurring on each 25th of december
             ],
         ]);
         $carbon::resetHolidays();
