41cac5a337b0296fc355b4ff9c483b444ab72d81
KyleK <kylekatarnls@gmail.com>
2019-04-04 08:01:35 +0200

    Add default class to type generator call

--- types.php
+++ types.php
@@ -5,6 +5,6 @@ include __DIR__.'/vendor/cmixin/business-day/src/Types/Generator.php';
 
 $sources = __DIR__.'/src';
 $generator = new \Types\Generator();
-$generator->writeHelpers($sources, $sources, '_ide_business_time', function () {
+$generator->writeHelpers(\Cmixin\BusinessTime::class, $sources, $sources, '_ide_business_time', function () {
     \Cmixin\BusinessTime::enable(\Carbon\Carbon::class);
 });
