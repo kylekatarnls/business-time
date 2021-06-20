6bb27da144310b10a9c6fd2387ec8d514bf3fa10
KyleKatarn <kylekatarnls@gmail.com>
2021-06-20 18:05:41 +0200

    Fix holidays region loading via Laravel config file

--- tests/Laravel/ServiceProviderTest.php
+++ tests/Laravel/ServiceProviderTest.php
@@ -4,6 +4,7 @@ namespace Tests\Carbon\Laravel;
 
 use BusinessTime\Laravel\ServiceProvider;
 use Carbon\Carbon;
+use Carbon\CarbonImmutable;
 use PHPUnit\Framework\TestCase;
 
 class ServiceProviderTest extends TestCase
@@ -34,5 +35,38 @@ class ServiceProviderTest extends TestCase
         $service->app->setHours(null);
 
         $this->assertNull($service->boot());
+        $this->assertNull(Carbon::getHolidaysRegion());
+    }
+
+    public function testConfig()
+    {
+        include_once __DIR__.'/ServiceProvider.php';
+        $service = new ServiceProvider();
+        $classes = [
+            Carbon::class,
+            CarbonImmutable::class,
+        ];
+
+        foreach ($classes as $class) {
+            $message = null;
+
+            $class::macro('getHolidaysRegion', null);
+
+            try {
+                $class::getHolidaysRegion();
+            } catch (\BadMethodCallException $e) {
+                $message = $e->getMessage();
+            }
+
+            $this->assertSame("Method $class::getHolidaysRegion does not exist.", $message);
+        }
+
+        $service->app->setHours(['holidays' => ['region' => 'au-qld']]);
+        $service->boot();
+        $service->register();
+
+        foreach ($classes as $class) {
+            $this->assertSame('au-qld', $class::getHolidaysRegion());
+        }
     }
 }
